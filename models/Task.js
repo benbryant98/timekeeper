19898b0267d4c76e63b2572e13e062b3ec56983d
Benjamin Bryant <bryantbc98@gmail.com>
2023-05-26 00:16:16 -0400

    changd a lot but the server runs, everything is visible when navigating to routes, work needs to be done so that user accounts can be created and a login is possible

--- models/Task.js
+++ models/Task.js
@@ -3,25 +3,36 @@ const sequelize = require("../config/connection");
 
 class Task extends Model {}
 
-Task.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Task.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    daily_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "daily",
+        key: "id",
+      },
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  body: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  sequelize,
-  timestamps: true,
-  freezeTableName: true,
-  modelName: "task",
-});
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "task",
+  }
+);
 
 module.exports = Task;
