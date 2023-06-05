6a4e050cd0f4df468cbaf970d069b49e7d70c250
Benjamin Bryant <bryantbc98@gmail.com>
2023-06-05 19:09:24 -0400

    finished setting up logic for handling new task creation and new user creation, adjusted models, routes, added handlebars for page elements

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,50 +1,46 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const task = document.querySelector('.task').value.trim();
-    const dayName = document.querySelector('#day-name').value.trim();
-    const dailyTask = document.querySelector('#daily-task').value.trim();
+  const title = document.querySelector("#title").value.trim();
+  const date = document.querySelector("#task-date").value;
+  const frequency = document.querySelector("#frequency").value;
+  const body = document.querySelector("#task-body").value.trim();
 
-    if (task & dayName && dailyTask) {
-        const response = await fetch(`/api/tasks`, {
-            method: 'POST',
-            body: JSON.stringify({ task, dayName, dailyTask}),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
+  if (title && frequency && date) {
+    const response = await fetch(`/api/tasks`, {
+      method: "POST",
+      body: JSON.stringify({ title, body, date, frequency }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (response.ok) {
-            document.location.replace('/profile')
-        } else {
-            alert('Failed to create daily task')
-        }
+    if (response.ok) {
+      console.log("test");
+      document.location.replace("/");
+    } else {
+      alert("Failed to create daily task");
     }
+  } else {
+    console.log(title + frequency + date);
+  }
 };
 
 const deleteButton = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
-        const response = await fetch(`/api/profile/${id}`, {
-            method: 'DELETE',
-        })
+    const response = await fetch(`/api/profile/${id}`, {
+      method: "DELETE",
+    });
 
-        if (response.ok) {
-            document.location.replace('/api/profile');
-        } else {
-            alert('Failed to delete task')
-        }
+    if (response.ok) {
+      document.location.replace("/api/profile");
+    } else {
+      alert("Failed to delete task");
     }
+  }
 };
 // My tutor and I console logged a lot of lines of code. He suggested this for the new task form to replace line 44-45. We tested it and it worked. The Create and Submit buttons went to the same place so only one is needed unless you have other plans for it.//
-// const newTaskForm = document.querySelector('.new-task-form')
-// newTaskForm.addEventListener("submit", newFormHandler);
-
-document.querySelector('.new-task-form')
-document.addEventListener('submit', newFormHandler);
-
-document.querySelector('.task-list')
-document.addEventListener('click', newFormHandler);
-
-
+const newTaskForm = document.getElementById("task-form");
+newTaskForm.addEventListener("submit", newFormHandler);
